4cd6b2ed6d0fba01ee7baff48610d1846109ad3a
Teun Schaeken <teun.schaeken@gmail.com>
2022-07-04 15:26:23 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useEffect } from "react";
+import { Map } from "./components/Map";
+import { HeaderPanel } from "./components/HeaderPanel";
+import { LandelijkPanel } from "./components/LandelijkPanel";
+import { ZiekenhuisPanel } from "./components/ZiekenhuisPanel";
+import { useStoreState, useStoreActions } from "./states/store";
+import { getHashFromUrl, loadFromHash, setHash } from "./processing/hash";
+import Typography from '@mui/material/Typography';
+import { AccordionRow, SimpleAccordionComponent } from "./components/SimpleAccordionComponent";
+import { loadAllSehFormsFromIndexDatabase } from "./states/sehdatabase";
+import { InformationIcon } from "./components/InformationIcon";
+import { EXTRA_DRUK_INFO } from "./texts";
+
+const App = () => {
+
+  const ziekenhuizen = useStoreState(state => state.ziekenhuizen)
+  const selectedZiekenhuis = useStoreState(state => state.selectedZiekenhuis)
+  const setZiekenhuisActiveOpenHour = useStoreActions(actions => actions.setZiekenhuisActiveOpenHour)
+  const setZiekenhuisActiveCloseHour = useStoreActions(actions => actions.setZiekenhuisActiveCloseHour)
+  const setZiekenhuisOpenDichtDagPersoneleBezetting = useStoreActions(actions => actions.setZiekenhuisOpenDichtDagPersoneleBezetting)
+  const loadSEHForm = useStoreActions(actions => actions.loadSEHForm)
+
+  useEffect(() => {
+    // load from hash
+    const hash = getHashFromUrl()
+    loadFromHash(
+      hash,
+      ziekenhuizen,
+      setZiekenhuisActiveOpenHour,
+      setZiekenhuisActiveCloseHour,
+      setZiekenhuisOpenDichtDagPersoneleBezetting
+    )
+    // load saved SEH forms
+    loadAllSehFormsFromIndexDatabase(loadSEHForm)
+  }, [])
+
+  // set hash
+  useEffect(() => {
+    setHash(ziekenhuizen)
+  }, [ziekenhuizen])
+
+  const accordionRows = [
+    {
+      header: (
+        <div className="flex flex-row gap-2">
+          <div className="font-bold italic">Extra druk op SEH locaties</div>,
+          <InformationIcon informationText={EXTRA_DRUK_INFO} />
+        </div>
+      ),
+      content: <LandelijkPanel />,
+      disabled: false
+    },
+    {
+      header: (selectedZiekenhuis === undefined) ?
+        <div className="">(selecteer een ziekenhuis op de kaart)</div> :
+        <div className="font-bold italic">{`${selectedZiekenhuis.locatie}`}</div>, // (${selectedZiekenhuis?.id})
+      content: selectedZiekenhuis && <ZiekenhuisPanel ziekenhuis={selectedZiekenhuis} />,
+      disabled: selectedZiekenhuis === undefined
+    }
+  ]
+
+  return (
+    <div className="App">
+
+      <Map />
+      <div className="p-2 sidepanel-container shadow-inner bg-neutral-100 flex flex-col gap-2">
+
+        <HeaderPanel />
+
+        <div className="mt-3">
+          <SimpleAccordionComponent rows={accordionRows} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/texts.ts
@@ -0,0 +1,18 @@
+export const UPLOAD_INFO = "Upload 1 of meer .json bestanden met SEH gegevens per ziekenhuis om op het dashboard te tonen."
+export const DOWNLOAD_INFO = "Download een .pdf bestand met het totaaloverzicht van alle beschikbaar gestelde data van de SEH’s."
+
+export const SLUITINGS_WINDOW_INFO = "Het sluitingswindow bepaalt op welk moment het ziekenhuis stopt met het aannemen van nieuwe SEH-patiënten tov de sluitingstijd."
+export const JAAR_SELECT_INFO = "Data van 2018 en 2019 is beschikbaar."
+export const VERVOERSKOSTEN_INFO = "Betreft vervoerskosten per patiënt obv opgave ambulancezorg Nederland. Vaste kosten per ambulancerit"
+export const BELADEN_KILOMETERS_INFO = "Betreft ambulancekosten per beladen km aanvullend op de vervoerskosten."
+
+export const EXTRA_DRUK_INFO = "Aantallen extra SEH bezoeken door (gedeeltelijke) sluiting SEH locaties. Uitgedrukt op landelijk en regionaal niveau"
+export const LANDELIJK_REGIONAAL_SELECT_INFO = "Selecteer weergave landelijk of regionaal niveau (ROAZ)."
+
+export const SALARISKOSTEN_BESPARING_INFO = "Salariskosten(besparing) op basis van personele bezetting die vrijvalt bij het bronziekenhuis"
+export const VERVOERSKOSTEN_BESPARING_INFO = "Betreft de kosten voor het terugleiden vervolgzorg naar “bronziekenhuis” bestaande uit ambulancekosten (vaste kosten + variable kosten voor elke beladen km)"
+
+export const OPENINGSTIJDEN_INFO = "De openingstijden kennen 3 presets; 24/7; Nachtsluiting en Sluiting. De doorrekening van klinische opnames en kosten worden bepaald door de presets in combinatie met de personele bezetting."
+export const SLUITING_CHECKBOX_INFO = "Voor volledige SEH sluiting moet een personele bezetting voor dag-, avond-, en nachtsituatie worden opgegeven.  Het is voor te stellen dat niet de volledige kernbezetting wegvalt bij sluiting vanwege dubbelfuncties van personeel. Dit kan hier worden meegenomen."
+export const NACHTSLUITING_CHECKBOX_INFO = "Voor de nachtsituatie staat de personele bezetting default op 1 arts-SEH en 2 SEH-vpk. Bij nachtsluiting op 1 arts-SEH en 1 SEH vpk. Dit kan aangepast worden per ziekenhuis."
+export const PERSONELE_BEZETTING_INFO = "De personele bezetting is vereenvoudigde weergave van de werkelijkheid. Onder arts-SEH vallen zowel een SEH-arts als poortspecialist met de benodigde ervaring op de SEH. Onder de categorie AIO/ANIOS valt ook eventueel ander personeel. Niet zijnde SEH vpk en art-SEH. De 3 categorieën zijn opgenomen om enige differentiatie in personeelskosten op te nemen."
